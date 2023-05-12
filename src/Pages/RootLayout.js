77a9581eb2f2a908f4ae2b15e15b667e076e9006
Wael <131612777+ggwael8@users.noreply.github.com>
2023-05-12 19:41:44 +0300

    removed userInfo from RootLayout

--- src/Pages/RootLayout.js
+++ src/Pages/RootLayout.js
@@ -1,43 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../component/NavBar/navbar';
-import { useState, useEffect } from 'react';
+import { useContext } from 'react';
 import UserContext from '../context/user-context';
 import { checkAuth } from '../utils/auth';
 function RootLayout() {
-  const [userInfo, setUserInfo] = useState();
-  const [dataFetched, setDataFetched] = useState(false);
-  //todo: fetch organization
-  async function fetchUserHandler() {
-    const response = await fetch(
-      'https://hospital-information-system-production-b18b.up.railway.app/auth/users/me/'
-    );
-    const data = await response.json();
-
-    const transformedInfo = {
-      role: data.role,
-      UserID: data.id,
-      PersonalInformation: {
-        title: 'Personal Information',
-        FirstName: data.first_name,
-        LastName: data.last_name,
-        Gender: data.gender === 'M' ? 'Male' : 'Female',
-        NationalId: data.national_id,
-        DateOfBirth: data.birth_date,
-      },
-      ContactInformation: {
-        title: 'Contact Information',
-        MobileNumber1: data.phone_1,
-        MobileNumber2: data.phone_2,
-        Email: data.email,
-      },
-    };
-    setUserInfo(transformedInfo);
-    setDataFetched(true);
-  }
-  useEffect(() => {
-    fetchUserHandler();
-  }, []);
-
+  const userctx = useContext(UserContext);
   const links = {
     patient: [
       {
@@ -114,30 +81,24 @@ function RootLayout() {
     ],
   };
   return (
-    dataFetched && (
-      <UserContext.Provider value={userInfo}>
-        {
-          <>
-            {checkAuth() && (
-              <NavBar
-                links={links[userInfo.role]}
-                id='nav'
-                firstname={userInfo.PersonalInformation.FirstName}
-                role={userInfo.role}
-              />
-            )}
-            <div
-              style={{
-                height: checkAuth() ? '90vh' : '100vh',
-                display: 'flex',
-              }}
-            >
-              <Outlet />
-            </div>
-          </>
-        }
-      </UserContext.Provider>
-    )
+    <>
+      {checkAuth() && (
+        <NavBar
+          links={links[userctx.role]}
+          id='nav'
+          firstname={userctx.PersonalInformation.FirstName}
+          role={userctx.role}
+        />
+      )}
+      <div
+        style={{
+          height: checkAuth() ? '90vh' : '100vh',
+          display: 'flex',
+        }}
+      >
+        <Outlet />
+      </div>
+    </>
   );
 }
 export default RootLayout;
