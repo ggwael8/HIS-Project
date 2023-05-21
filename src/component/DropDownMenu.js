db70d49dc39cc61234813ffbb2888e37a6990ae5
Wael <131612777+ggwael8@users.noreply.github.com>
2023-05-22 01:48:25 +0300

    added more functionality

--- src/component/DropDownMenu.js
+++ src/component/DropDownMenu.js
@@ -45,26 +45,47 @@ function DropDownMenu(props) {
               >
                 {p.body}
               </div>
-              {/* {<hr></hr>} */}
             </>
           ) : (
             props.type === 'card' && (
               <>
                 <div
                   onClick={() => {
-                    props.selectstate(p.id);
+                    if (props.multiple) {
+                      // if multiple selection is allowed
+                      props.selectstate(prevState => {
+                        if (prevState.includes(p.id)) {
+                          // if the ID already exists in the array, remove it
+                          return prevState.filter(id => id !== p.id);
+                        } else {
+                          // if the ID doesn't exist in the array, add it
+                          return [...prevState, p.id];
+                        }
+                      });
+                    } else {
+                      // if only single selection is allowed
+                      props.selectstate(p.id);
+                      props.clickFunction &&
+                        props.clickFunction(p.id, p.card.name);
+                    }
                     p.day && props.selectedDay(p.body);
-                    props.setSelectedStep(props.currentStep + 1);
+                    props.currentStep &&
+                      props.setSelectedStep(props.currentStep + 1);
                   }}
                   className={`${classes.cardContainer} `}
                 >
-                  <div className={`${classes.card} ${classes.hoverEffect}`}>
+                  <div
+                    className={`${classes.card} ${classes.hoverEffect} ${
+                      Array.isArray(props.selected) &&
+                      props.selected.includes(p.id) &&
+                      classes.selected
+                    }`}
+                  >
                     {Object.keys(p.card).map(key => {
                       return <>{p.card[key]}</>;
                     })}
                   </div>
                 </div>
-                {/* {<hr></hr>} */}
               </>
             )
           )}
