d9f707a5d52db9dbc93c3adc8c3c2fe32fa1d7e6
Wael <131612777+ggwael8@users.noreply.github.com>
2023-05-21 04:35:52 +0300

    more functionality added to details body component

--- src/component/DetailsBody/DetailsBody.js
+++ src/component/DetailsBody/DetailsBody.js
@@ -2,11 +2,22 @@ import classes from './DetailsBody.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 function DetailsBody(props) {
+  let values;
   return (
     <div className={classes.details} style={props.style}>
       <h2 className={classes.title}>{props.title}</h2>
       <div className={classes.detailsContainer}>
         <div className={classes.detailsHeader}>
+          {props.additionalFunction && (
+            <button
+              className={classes.Link}
+              onClick={() => {
+                props.additionalFunction.setStates();
+              }}
+            >
+              {props.additionalFunction.title}
+            </button>
+          )}
           <input type='text' id='search' placeholder='search' />
           <span></span>
           <div
@@ -26,32 +37,65 @@ function DetailsBody(props) {
           {props.details.map(details => {
             return (
               <div className={classes.detailsBodyContent}>
-                <div className={classes.detailsBodyContentData}>
-                  {Object.keys(details).map(
-                    a =>
-                      a !== 'button' && (
-                        <h4>
-                          {a} : {details[a]}
-                        </h4>
-                      )
-                  )}
-                </div>
-                <div className={classes.buttons}>
-                {Object.keys(details).map(
-                  info =>
-                    info === 'button' &&
-                    details[info].map(info => {
-                      return (
-                        <button
-                          className={classes.Button}
-                          onClick={info.setStates}
-                        >
-                          {info.title}
-                        </button>
-                      );
-                    })
+                {props.thereIsCard ? (
+                  Object.keys(details).map(
+                    info =>
+                      info === 'cards' &&
+                      Object.keys(details[info]).map(cardInfo => {
+                        return (
+                          <>
+                            <div className={classes.detailsBodyContentData}>
+                              {cardInfo === 'title' ? (
+                                <div
+                                  className={
+                                    classes.detailsBodyContentDataTitle
+                                  }
+                                >
+                                  <h3>{details[info][cardInfo]}</h3>
+                                </div>
+                              ) : cardInfo === 'body' ? (
+                                <></>
+                              ) : (
+                                <h5>
+                                  {cardInfo} : {details[info][cardInfo]}
+                                </h5>
+                              )}
+                            </div>
+                          </>
+                        );
+                      })
+                  )
+                ) : (
+                  <>
+                    <div className={classes.detailsBodyContentData}>
+                      {Object.keys(details).map(
+                        a =>
+                          a !== 'button' &&
+                          a !== 'cards' && (
+                            <h4>
+                              {a} : {details[a]}
+                            </h4>
+                          )
+                      )}
+                    </div>
+                    <div className={classes.buttons}>
+                      {Object.keys(details).map(
+                        info =>
+                          info === 'button' &&
+                          details[info].map(info => {
+                            return (
+                              <button
+                                className={classes.Button}
+                                onClick={info.setStates}
+                              >
+                                {info.title}
+                              </button>
+                            );
+                          })
+                      )}
+                    </div>
+                  </>
                 )}
-                </div>
               </div>
             );
           })}
